e27226d9cd4dfb33902877750a6f80bc036da4d2
arnold-oghiator <arnoldoghiator@gmail.com>
2022-07-19 11:27:45 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import List from './component/List';
 
 function App() {
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-text'>
+      <h1>Simple Crud App</h1>
+
+      <form onSubmit={handleSubmit} >
+
+        <div >
+          <input type="text" className='input-text' placeholder='name....' />
+     
+      
+          <button type="submit"> Submit</button>
+        </div>
+      </form>
+
+    <List/>
     </div>
   );
 }
---
+++ src/component/List.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BsTrash } from "react-icons/bs";
+import { FaRegEdit } from "react-icons/fa";
+
+export default function List() {
+  return (
+    <div>
+        <div>
+            <div>
+                name
+            </div>
+            <div>
+            <BsTrash/>
+            <FaRegEdit/>
+            </div>
+        </div>
+
+
+
+      
+    </div>
+  )
+}
